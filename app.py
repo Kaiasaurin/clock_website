0e8d4a3abc3f4caec479392ed8b8cfab36ecfb16
Kaiasaurin <101678546+Kaiasaurin@users.noreply.github.com>
2023-07-26 00:34:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask, json
-from datetime import datetime
+from flask import Flask, render_template, jsonify
+import datetime
 import pytz
 
 app = Flask(__name__)
 
-time_zones = {
-    "PST": "US/Pacific",
-    "MST": "US/Mountain",
-    "CST": "US/Central",
-    "EST": "US/Eastern",
-    "UTC": "UTC"
-}
+@app.route('/')
+def index():
+    time_zones_data = get_time_zones()
+    return render_template('clock.html', time_zones = time_zones_data)
 
-@app.route("/time")
+@app.route('/time')
 def time():
-    response = {}
-    for tz_short, tz in time_zones.items():
-        now = datetime.now(pytz.timezone(tz))
-        response[tz_short] = now.strftime("%Y-%m-%d %I:%M:%S %p")
-    return json.dumps(response)
+    time_zones_data = get_time_zones()
+    return jsonify(time_zones_data)
+
+def get_time_zones():
+    time_zones = ["US/Pacific", "US/Mountain", "US/Central", "US/Eastern", "UTC"]
+    time_zones_data = {}
+    for tz in time_zones:
+        now = datetime.datetime.now(pytz.timezone(tz)).strftime("%Y-%m-%d %I:%M:%S %p")
+        time_zones_data[tz] = now
+    return time_zones_data
+    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4000)
