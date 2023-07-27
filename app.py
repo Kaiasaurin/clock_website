6196399ea01d59882dc136ae1694e44e4edd817f
Kaiasaurin <Kaiasaurin@gmail.com>
2023-07-26 18:55:54 -0700

    GMT+ Not Working

--- app.py
+++ app.py
@@ -33,10 +33,10 @@ def get_time_zones():
     # GMT time zones
     "Etc/GMT", "Etc/GMT+1", "Etc/GMT+2", "Etc/GMT+3", "Etc/GMT+4", "Etc/GMT+5", 
     "Etc/GMT+6", "Etc/GMT+7", "Etc/GMT+8", "Etc/GMT+9", "Etc/GMT+10", 
-    "Etc/GMT+11", "Etc/GMT+12", "Etc/GMT+13", "Etc/GMT+14",
+    "Etc/GMT+11", "Etc/GMT+12",
     "Etc/GMT-1", "Etc/GMT-2", "Etc/GMT-3", "Etc/GMT-4", "Etc/GMT-5", 
     "Etc/GMT-6", "Etc/GMT-7", "Etc/GMT-8", "Etc/GMT-9", "Etc/GMT-10", 
-    "Etc/GMT-11", "Etc/GMT-12", "Etc/GMT-13", "Etc/GMT-14"
+    "Etc/GMT-11", "Etc/GMT-12"
 ]
     time_zones_data = {}
     for tz in time_zones:
