a0f1a0a30aaa2cc5224775a91e9ddd0d64a0ff20
Kaiasaurin <Kaiasaurin@gmail.com>
2023-07-26 18:32:24 -0700

    Added time zones

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ def time():
     return jsonify(time_zones_data)
 
 def get_time_zones():
-    time_zones = ["US/Pacific", "US/Mountain", "US/Central", "US/Eastern", "Europe/Lisbon", "Europe/London", "Europe/Paris", "Africa/Cairo", "Asia/Tokyo"]
+    time_zones = [
+        "US/Pacific", "US/Mountain", "US/Central", "US/Eastern",
+        "Pacific/Auckland", "Australia/Melbourne", "Australia/Sydney", "Australia/Darwin", "Australia/Perth",
+        "Asia/Seoul", "Asia/Shanghai", "Asia/Beijing", "Asia/Jerusalem", "Asia/Singapore", "Asia/Bangkok", "Asia/Tokyo", "Pacific/Fiji",
+        "Europe/Berlin", "Europe/Moscow", "Europe/Rome", "Europe/Kiev", "Europe/Zurich", "Europe/Madrid", "Europe/Lisbon", "Europe/Paris", "Europe/London"        
+    ]
     time_zones_data = {}
     for tz in time_zones:
         now = datetime.datetime.now(pytz.timezone(tz)).strftime("%Y-%m-%d %I:%M:%S %p")
