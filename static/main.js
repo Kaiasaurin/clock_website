d43ff6d33bf48cdeedbd8cc40e7fffe4a32c5970
Kaiasaurin <Kaiasaurin@gmail.com>
2023-07-26 17:14:32 -0700

    detect mobile

--- static/main.js
+++ static/main.js
@@ -6,3 +6,18 @@ function fetchTime() {
     });
 }
 setInterval(fetchTime, 1000);
+
+function detectDevice() {
+    if (/iPad|iPhone|iPod/.test(navigator.userAgent) || /iPad|iPhone|iPod/.test(navigator.platform)) {
+        return "ios";
+    } else if (window.matchMedia("(max-width: 767px)").matches) {
+        return "mobile";
+    } else {
+        return "desktop";
+    }
+}
+
+$(document).ready(function() {
+    var deviceType = detectDevice();
+    $("body").addClass(deviceType);
+});
