eeecb29a4d26c818001b8680a55045ece686e2fb
Kaiasaurin <Kaiasaurin@gmail.com>
2023-07-26 17:18:53 -0700

    1

--- static/main.js
+++ static/main.js
@@ -1,14 +1,5 @@
-function fetchTime() {  
-    $.getJSON("/time", function(data){
-        for (var key in data) {
-            $("#" + key).html(key.replace("-", "/") + ": " + data[key]);  // Append the time zone name to the current time
-        }   
-    });
-}
-setInterval(fetchTime, 1000);
-
 function detectDevice() {
-    if (/iPad|iPhone|iPod/.test(navigator.userAgent) || /iPad|iPhone|iPod/.test(navigator.platform)) {
+    if (/iPad|iPhone|iPod/.test(navigator.userAgent) || (/iPad|iPhone|iPod/.test(navigator.platform)) || (navigator.maxTouchPoints && navigator.maxTouchPoints > 2 && /MacIntel/.test(navigator.platform))) {
         return "ios";
     } else if (window.matchMedia("(max-width: 767px)").matches) {
         return "mobile";
@@ -17,7 +8,16 @@ function detectDevice() {
     }
 }
 
+function fetchTime() {  
+    $.getJSON("/time", function(data){
+        for (var key in data) {
+            $("#" + key).html(key.replace("-", "/") + ": " + data[key]);  // Append the time zone name to the current time
+        }   
+    });
+}
+
 $(document).ready(function() {
     var deviceType = detectDevice();
     $("body").addClass(deviceType);
+    setInterval(fetchTime, 1000);
 });
